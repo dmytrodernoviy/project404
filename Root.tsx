c25747403f9a69bf90647f640609387fb923c7d8
Dmytro Dernoviy <dernovoi.d.lightit@gmail.com>
2020-04-02 15:53:20 +0300

    added splash screen for iOS

--- Root.tsx
+++ Root.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useRef } from 'react';
 import AppStack from '@src/navigation/AppStack';
+import SplashScreen from 'react-native-splash-screen';
 import { NavigationService } from '@src/services';
 
-const Root = () => {
+const Root: React.FC = () => {
   const appStack = useRef(null);
   useEffect(() => {
     NavigationService.setTopLevelNavigator(appStack.current);
+    SplashScreen.hide();
   }, []);
 
   return <AppStack ref={appStack} />;
